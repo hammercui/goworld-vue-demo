6c3ccd20297874a0c4dbe30cadb8a4561230a40d
sir <hammercui@163.com>
2022-03-21 18:07:33 +0800

    add: tick

--- lib/goworld/core/ts/Packet.ts
+++ lib/goworld/core/ts/Packet.ts
@@ -1,3 +1,16 @@
 /**
  * 发送包
  */
+
+export default class Packet{
+
+
+    public constructor(mt: number){
+
+    }
+
+    public static New(mt: number): Packet{
+        let packet = new Packet(mt);
+        return packet;
+    }
+}
